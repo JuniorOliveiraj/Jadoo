e8f139312e718cbae909dc539fe8c0a5441aae2f
Junior <junioroliveira.belem@gmail.com>
2024-06-24 09:02:10 -0300

    sem seting de color

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ export default function App() {
       <ThemeProvider>
         <ScrollbarStyle>
           <ScrollToTop />
-          <Settings />
+          {/* <Settings /> */}
           <Router />
         </ScrollbarStyle>
       </ThemeProvider>
