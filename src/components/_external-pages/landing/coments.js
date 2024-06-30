1228071932d7f8f6fb335beb86cc4a721a477bf9
Junior Mac <junioroliveira.belem@gmail.com>
2024-06-30 01:09:12 -0300

    alterar coment vew with, onclick

--- src/components/_external-pages/landing/coments.js
+++ src/components/_external-pages/landing/coments.js
@@ -1,20 +1,29 @@
 // material
-import { useTheme, styled } from '@mui/material';
+import { Card, styled } from '@mui/material';
 import { Box, Grid, Container, Typography, Stack, } from '@mui/material';
 //
-import { MotionInView, varFadeInDown, varFadeInRight, fadeInUp } from '../../animate';
+import { MotionInView, varFadeInDown, varFadeInRight,varFadeInUp } from '../../animate';
 import { motion } from 'framer-motion';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import { useState } from 'react';
 
 // ----------------------------------------------------------------------
 
 
 
+const CoverImgStyle = styled('img')({
+    top: 0,
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+    WebkitUserDrag: 'none',
+    MozUserDrag: 'none',
+    MsUserDrag: 'none',
+    userDrag: 'none',
+    cursor: 'pointer'
 
-const Cards = [1, 2, 3]
+});
+
+const Cards = [0, 1, 2, 3, 4]
 
 const RootStyle = styled('div')(({ theme }) => ({
     width: "100%",
@@ -36,6 +45,27 @@ const HeroOverlayStyle = styled(motion.img)({
 // ----------------------------------------------------------------------
 
 export default function Coments() {
+    const [value, setValue] = useState(2);
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`tabpanel-${index}`}
+                aria-labelledby={`tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box sx={{ p: 3 }}>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
     return (
         <RootStyle>
             <Container maxWidth="lg" >
@@ -47,12 +77,12 @@ export default function Coments() {
                     <Grid item xs={10} md={5}>
                         <motion.div variants={varFadeInRight}>
                             <Typography variant="b">
-                            Testimonials
+                                Testimonials
                             </Typography>
                             <br />
                             <Typography variant="h2" >
-                            O que as pessoas dizem
-                            sobre nós.
+                                O que as pessoas dizem
+                                sobre nós.
 
                             </Typography>
                         </motion.div>
@@ -67,23 +97,44 @@ export default function Coments() {
                     <Grid item xs={10} md={6}>
                         <Stack spacing={5} direction={{ xs: 'column', md: 'row' }} justifyContent="space-between">
                             <Container maxWidth="lg" sx={{ position: 'relative', textAlign: 'center' }}>
-                                <Box sx={{ position: 'relative' }}>
-                                    <Box sx={{ position: 'absolute', top: 0 }}>
-                                        <Box sx={{ position: 'relative', zIndex: 9 }}>
-                                            <MotionInView variants={varFadeInDown}>
-                                                <motion.div animate={{ y: [0, -20, 0] }} transition={{ duration: 8, repeat: Infinity }}>
-                                                    <img alt="sidebar" src={`/static/mock-images/imageHome/coment1.png`} />
-                                                </motion.div>
-                                            </MotionInView>
-                                        </Box>
-                                    </Box>
+                                <Box sx={{ width: '100%' }}>
+
+                                    {/* Painéis das Abas */}
+                                    {Cards.map((tab, index) => (
+                                        <TabPanel key={index} value={value} index={index}>
+                                            <Box sx={{ position: 'relative' }}>
+                                                <Box sx={{ position: 'absolute', top: 0 }}>
+                                                    <Box sx={{ position: 'relative', zIndex: 9 }}>
+                                                        <MotionInView variants={varFadeInDown}>
+                                                            <motion.div animate={{ y: [0, -20, 0] }} transition={{ duration: 8, repeat: Infinity }}>
+                                                                <img alt="sidebar" src={`/static/mock-images/imageHome/coment1.png`} />
+                                                            </motion.div>
+                                                        </MotionInView>
+                                                    </Box>
+                                                    <Box sx={{ position: 'absolute', top: 0 }}>
+                                                        <MotionInView variants={varFadeInUp}>
+                                                            <motion.div animate={{ y: [-10, 10, -10] }} transition={{ duration: 8, repeat: Infinity }}>
+                                                                <img alt="sidebar" src={`/static/mock-images/imageHome/coment0.png`} />
+                                                            </motion.div>
+                                                        </MotionInView>
+                                                    </Box>
+                                                </Box>
+                                            </Box>
+                                        </TabPanel>
+                                    ))}
                                 </Box>
                             </Container>
                         </Stack>
                     </Grid>
                 </Grid>
-                <Box sx={{height: 100}}/>
-                <img alt="sidebar" src={`/static/mock-images/imageHome/logosmark.png`} style={{marginTop:60}}/>
+
+
+                <Box sx={{ height: 100 }} />
+                <Box sx={{ display: "flex", marginTop: 10 }}>
+                    {Cards.map((tab, index) => (
+                        <Card onClick={() => { setValue(index) }} sx={{ width: "100%", margin: 2, boxShadow: value != index && 'none' }}><CoverImgStyle sx={{ filter: value != index && 'grayscale(100%)' }} alt="sidebar" src={`/static/mock-images/imageHome/marca${index}.png`} /></Card>
+                    ))}
+                </Box>
             </Container>
         </RootStyle>
     );
